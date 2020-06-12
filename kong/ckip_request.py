8eeb289b9adc2f987b3928526c8d04a4dce571b6
JimboChien <jim60507@gmail.com>
2020-06-12 23:39:48 +0800

    Add CKIP NLP

---
+++ kong/ckip_request.py
@@ -0,0 +1,24 @@
+import json
+import requests
+import sys
+
+url = 'https://ckip.iis.sinica.edu.tw/api/corenlp/?ner'
+
+input = sys.argv[1]
+
+header = {
+  'Content-Type': 'application/json; charset=utf8'
+}
+
+payload = {
+  'text': input
+}
+
+r = requests.post(url, data=json.dumps(payload), headers = header)
+print(r.json())
+datas = r.json()['ner'][0]
+
+for data in datas:
+  print(data)
+#  if data['ner'] == 'GPE':
+#    print(data['text'])
