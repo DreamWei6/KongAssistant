41ae35c2de9ebde5049e0b2434690030ed9f0d13
JimboChien <jim60507@gmail.com>
2020-06-13 04:39:32 +0800

    Add Ckip WS Test

--- kong/ckip_request.py
+++ kong/ckip_request.py
@@ -2,7 +2,7 @@ import json
 import requests
 import sys
 
-url = 'https://ckip.iis.sinica.edu.tw/api/corenlp/?ner'
+url = 'https://ckip.iis.sinica.edu.tw/api/corenlp/?ner&ws'
 
 input = sys.argv[1]
 
@@ -17,6 +17,11 @@ payload = {
 r = requests.post(url, data=json.dumps(payload), headers = header)
 print(r.json())
 datas = r.json()['ner'][0]
+
+for data in datas:
+  print(data)
+
+datas = r.json()['ws'][0]
 
 for data in datas:
   print(data)
